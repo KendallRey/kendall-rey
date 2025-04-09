5b5dfac925f0595f6c83bab52e86a24ef8832a57
UVMapping <107897863+KendallRey@users.noreply.github.com>
2025-04-09 20:53:53 +0800

    added PROJECT_TYPE enums

---
+++ src/constants/enums.ts
@@ -0,0 +1,12 @@
+export const PROJECT_TYPE = {
+  LEARNING: {
+    id: 'learning',
+    className: 'text-neutral-100 border-gray-700 bg-slate-800',
+    name: 'Learning',
+  },
+  HOBBY: {
+    id: 'hobby',
+    className: 'text-neutral-100 border-gray-700 bg-slate-800',
+    name: 'Hobby',
+  },
+};
