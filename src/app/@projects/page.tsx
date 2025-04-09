5680a0f58cf2feeff257303b937a1cc5ad51cb20
UVMapping <107897863+KendallRey@users.noreply.github.com>
2025-04-09 20:54:34 +0800

    update Projects section

--- src/app/@projects/page.tsx
+++ src/app/@projects/page.tsx
@@ -1,27 +1,55 @@
 'use client';
 
-import { Section } from '@/components';
+import { Chip, Section } from '@/components';
+import { PROJECT_TYPE } from '@/constants/enums';
+import { TECH } from '@/constants/techs';
 import { useIntersectionObserver } from '@/hooks/useIntersectionObserver';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const projects = [
   {
-    title: 'Project One',
+    title: 'Mui Component Generator',
     description:
-      'A placeholder project description. This project focuses on UI/UX.',
-    link: '#',
-    image: 'https://via.placeholder.com/600x400',
+      'A generator for MUI components in ReactJS, to kickstart your customizable components.',
+    link: 'https://www.npmjs.com/package/mui-component-generator',
+    image: '/mui-component-generator.png',
+    techs: [TECH.TYPESCRIPT, TECH.NPM],
+    type: PROJECT_TYPE.LEARNING,
   },
   {
-    title: 'Project Two',
-    description: 'Another project about front-end development and animations.',
-    link: '#',
-    image: 'https://via.placeholder.com/600x400',
+    title: 'Manga List',
+    description:
+      'A simple manga manager to record, search, and organize any titles.',
+    link: 'https://github.com/KendallRey/manga-list',
+    image: '/manga-list.png',
+    techs: [
+      TECH.TYPESCRIPT,
+      TECH.NEXTJS,
+      TECH.MATERIAL_UI,
+      TECH.DRIZZLE,
+      TECH.TAILWIND,
+      TECH.SUPABASE,
+      TECH.POSTGRESQL,
+      TECH.REDUX,
+      TECH.VERCEL,
+    ],
+    type: PROJECT_TYPE.HOBBY,
   },
   {
-    title: 'Project Three',
-    description: 'A mobile app project built with React Native and Expo.',
-    link: '#',
-    image: 'https://via.placeholder.com/600x400',
+    title: 'Portfolio 3D',
+    description:
+      'A sample portfolio built to explore 3D integration in React using Three.js.',
+    link: 'https://nx-mono-coral.vercel.app/',
+    image: '/portfolio-2024.png',
+    techs: [
+      TECH.TYPESCRIPT,
+      TECH.NEXTJS,
+      TECH.TAILWIND,
+      TECH.THREE_JS,
+      TECH.BLENDER_3D,
+    ],
+    type: PROJECT_TYPE.LEARNING,
   },
 ];
 
@@ -43,31 +71,61 @@ const page = () => {
   return (
     <Section
       id="projects"
-      className="min-h-screen flex flex-col bg-gray-900 text-white  md:p-12"
+      className="min-h-screen flex flex-col bg-gray-900 text-white md:p-12"
     >
-      <div className="max-w-6xl mx-auto px-6">
+      <div className="max-w-6xl mx-auto px-6 py-12 md:py-0">
         <h2 className="section-title text-4xl font-semibold text-center transition-all duration-700 mb-12">
           Projects
         </h2>
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
           {projects.map((project, index) => (
-            <a
+            <Link
               key={index}
               href={project.link}
               target="_blank"
               rel="noopener noreferrer"
               className="project-card translate-y-10 transition-all duration-700 bg-gray-800 rounded-lg overflow-hidden shadow-lg hover:scale-[1.02] hover:shadow-xl"
             >
-              <img
-                src={project.image}
-                alt={project.title}
-                className="w-full h-48 object-cover"
-              />
-              <div className="p-4">
+              <div className="w-full h-48 flex items-center relative">
+                <Image
+                  src={project.image}
+                  alt={project.title}
+                  fill
+                  priority
+                  quality={20}
+                  className="object-cover"
+                  style={{ filter: 'blur(8px)' }}
+                />
+                <Image
+                  src={project.image}
+                  alt={project.title}
+                  width={392}
+                  height={150}
+                  className="z-10 px-2"
+                />
+              </div>
+              <div className="relative mt-4 p-4 border-t border-gray-700 flex flex-col gap-1">
+                {project.type && (
+                  <Chip
+                    className={`${project.type.className} absolute right-[16px] top-[-16px]`}
+                  >
+                    {project.type.name}
+                  </Chip>
+                )}
                 <h3 className="text-xl font-bold">{project.title}</h3>
                 <p className="text-gray-400">{project.description}</p>
+                <div className="flex gap-2 flex-wrap">
+                  {project.techs?.map((tech) => (
+                    <Chip
+                      key={tech.name}
+                      className={`tech-chip transition-all duration-700 ${tech.className || ''}`}
+                    >
+                      {tech.name}
+                    </Chip>
+                  ))}
+                </div>
               </div>
-            </a>
+            </Link>
           ))}
         </div>
       </div>
