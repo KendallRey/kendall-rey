9bd3587b352a8152b162263b2e76e3cf0819c5fd
UVMapping <107897863+KendallRey@users.noreply.github.com>
2025-04-09 17:29:47 +0800

    added TECH constants

---
+++ src/constants/techs.ts
@@ -0,0 +1,27 @@
+type TechType = {
+  name: string;
+  className: string;
+}
+
+export const TECH: Record<string, TechType> = {
+  AXIOS: { className: 'text-[#671ddf] border-[#671ddf]', name: 'Axios' },
+  BOOTSTRAP: { className: 'text-[#6f2cf3] border-[#6f2cf3]', name: 'Bootstrap CSS' },
+  DJANGO: { className: 'text-[#ffffff] border-[#ffffff] bg-[#092e20]', name: 'Django Python' },
+  DRIZZLE: { className: 'text-[#c5f74f] border-[#c5f74f]', name: 'Drizzle ORM' },
+  GITHUB: { className: 'text-[#f0f6fc] border-[#f0f6fc] bg-[#010409]', name: 'Github' },
+  GOOGLE_FIREBASE: { className: 'text-[#ff9408] border-[#ff9408]', name: 'Firebase'},
+  JAVASCRIPT: { className: 'text-[#f7e018] border-[#f7e018]', name: 'Javascript' },
+  JIRA: { className: 'text-[#1868db] border-[#1868db]', name: 'Jira' },
+  MATERIAL_UI: { className: 'text-[#007fff] border-[#007fff]', name: 'Material UI' },
+  POSTGRESQL: { className: 'text-[#336791] border-[#336791]', name: 'PostgreSQL' },
+  REACT: { className: 'text-[#58c4dc] border-[#58c4dc]', name: 'ReactJS' },
+  REACT_NATIVE: { className: 'text-[#71d6ec] border-[#71d6ec]', name: 'React Native' },
+  REDUX: { className: 'text-[#593d88] border-[#593d88]', name: 'Redux Toolkit' },
+  SASS: { className: 'text-[#ce679a] border-[#ce679a]', name: 'SASS' },
+  SUPABASE: { className: 'text-[#3ecd8d] border-[#3ecd8d]', name: 'SASS' },
+  TAILWIND: { className: 'text-[#00bcff] border-[#00bcff]', name: 'Tailwind CSS' },
+  TYPESCRIPT: { className: 'text-[#3178c6] border-[#3178c6]', name: 'Typescript' },
+  VITE: { className: 'text-[#9499ff] border-[#9499ff]', name: 'Vite' },
+  VSCODE: { className: 'text-[#24aef5] border-[#24aef5]', name: 'VS Code' },
+  ZOD: { className: 'text-[#42b983] border-[#42b983]', name: 'Zod' },
+};
